ab3c5bbdbdaf7861d5ba111821763e523c4128ef
lcjne00 <lcjne00@gmail.com>
2022-08-22 21:12:24 +0900

    print TDA

---
+++ src/main/java/Testgit/demo/controller/TodoController.java
@@ -0,0 +1,15 @@
+package Testgit.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "to-do Application !";
+    }
+}
--- src/test/java/Testgit/demo/DemoApplicationTests.java
+++ src/test/java/Testgit/demo/DemoApplicationTests.java
@@ -1,13 +1,15 @@
 package Testgit.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public String helloWorld() {
+		return "to-do Application !";
 	}
 
 }
